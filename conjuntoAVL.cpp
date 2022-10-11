90321a4481ff0bc60988147280a35e6c2240bb0f
Lukaze <lasotomayor20@gmail.com>
2022-10-11 01:56:18 -0300

    Leaks arreglados

--- conjuntoAVL.cpp
+++ conjuntoAVL.cpp
@@ -40,19 +40,19 @@ private: //Funciones necesarias para el funcionamiento del AVL pero no para el u
     NodoAVL<T>* _raiz;
     unsigned int _cardinal;
     void destruir(NodoAVL<T>* raiz);
-    void rebalancear(NodoAVL<T>* n);
-    void definirBalanceo(NodoAVL<T>* n);
+    void rebalancear(NodoAVL<T>* nodo);
+    void definirBalanceo(NodoAVL<T>* nodo);
     NodoAVL<T>* rotacionIzquierda (NodoAVL<T>* a);
     NodoAVL<T>* rotacionDerecha (NodoAVL<T>* a);
     NodoAVL<T>* rotacionIzqLuegoDer (NodoAVL<T>* n);
-    NodoAVL<T>* rotacionDerLuegoIzq (NodoAVL<T>* n);
-    int largo (NodoAVL<T>* n);
-    void removerHoja(NodoAVL<T> *arClave, NodoAVL<T> *padreClave); //Sacados del taller del ABB
-    void removerConUnHijo(NodoAVL<T>* arClave, NodoAVL<T> *padreClave);
-    void removerConDosHijos(NodoAVL<T> *arClave);
+    NodoAVL<T>* rotacionDerLuegoIzq (NodoAVL<T>* nodo);
+    int largo (NodoAVL<T>* nodo);
+    void removerHoja(NodoAVL<T> *nodoBorrar, NodoAVL<T> *padreNodo); //Sacados del taller del ABB
+    void removerConUnHijo(NodoAVL<T>* nodoBorrar, NodoAVL<T> *padreNodo);
+    void removerConDosHijos(NodoAVL<T> *nodoBorrar);
     NodoAVL<T>* subArbolPadre(NodoAVL<T> *raiz, const T &clave);
-    NodoAVL<T>* maximoDeArbol(NodoAVL<T> *n);
-    NodoAVL<T>* predecesorMaximo(NodoAVL<T> *abb);
+    NodoAVL<T>* maximoDeArbol(NodoAVL<T> *nodoRaiz);
+    NodoAVL<T>* predecesorMaximo(NodoAVL<T> *nodo);
     void printAVL(NodoAVL<T>* root, int space);
 };
 /***************************************Defino funciones publicas******************************************/
@@ -75,40 +75,32 @@ unsigned int ConjuntoAVL<T>::cardinal() const {
 
 template <class T>
 bool ConjuntoAVL<T>::pertenece(const T& clave) const {
-    NodoAVL<T>* subArbolActual = _raiz;
-    while (subArbolActual != nullptr && subArbolActual->clave != clave){
-        if (clave < subArbolActual->clave){
-            subArbolActual = subArbolActual->izquierda;
-        } else if (subArbolActual->clave < clave){
-            subArbolActual = subArbolActual->derecha;
-        }
-    }
-    return subArbolActual != nullptr && subArbolActual->clave == clave;
+    NodoAVL<T>* nodo = _raiz;
+    while (nodo != nullptr && nodo->clave != clave)
+        (clave < nodo->clave) ? nodo = nodo->izquierda : nodo = nodo->derecha;
+    return nodo != nullptr && nodo->clave == clave;
 }
 
 template <class T>
 void ConjuntoAVL<T>::insertar(const T& clave){
-    if (cardinal() == 0){
-        _raiz = new NodoAVL<T>(clave, nullptr);
-        _cardinal++;
-    }
-        //Podria mejorarse para no evaluar en log n si pertenece o no la clave.
-    else if (!pertenece(clave)){
-        NodoAVL<T> *n = _raiz;
+    if (cardinal() == 0) _raiz = new NodoAVL<T>(clave, nullptr);
+    else {
+        NodoAVL<T> *nodo = _raiz;
         NodoAVL<T> *padre;
         bool agregado = false;
         while (!agregado) {
-            padre = n;
-            bool irIzquierda = clave < n->clave;
-            n = irIzquierda ? n->izquierda : n->derecha;
-            if (n==nullptr){
+            padre = nodo;
+            bool irIzquierda = clave < nodo->clave;
+            nodo->clave == clave ? agregado = true : agregado = false;
+            nodo = irIzquierda ? nodo->izquierda : nodo->derecha;
+            if (nodo==nullptr){
                 irIzquierda ? padre->izquierda = new NodoAVL<T>(clave,padre) : padre->derecha = new NodoAVL<T>(clave,padre);
                 agregado = true;
                 rebalancear(padre);
             }
         }
-        _cardinal++;
     }
+    _cardinal++;
 }
 
 //Hago el borrado del Nodo, verifico si el arbol existe y luego busco el nodo. Si el elemento esta
@@ -119,42 +111,34 @@ void ConjuntoAVL<T>::insertar(const T& clave){
 template<class T>
 void ConjuntoAVL<T>::borrar(const T& clave){
     if (_raiz == nullptr) return;
-    NodoAVL<T> *n = _raiz;
+    NodoAVL<T> *nodo = _raiz;
     NodoAVL<T> *padre = _raiz;
-    NodoAVL<T> *nodoABorrar = nullptr;
     NodoAVL<T> *hijo = _raiz;
-    while(hijo != nullptr && nodoABorrar == nullptr){ //Tiene la desventajas que siempre itera hasta el final LOL
-        padre = n;
-        n = hijo;
-        hijo = clave < n->clave ? n->izquierda : n->derecha;
-        if (clave == n->clave) nodoABorrar = n;
+    while(hijo != nullptr && nodo->clave != clave){ //Itero hasta encontrar el nodo con la clave a borrar
+        padre = nodo; nodo = hijo;
+        hijo = clave < nodo->clave ? nodo->izquierda : nodo->derecha;
     }
-    if (nodoABorrar->izquierda== nullptr && nodoABorrar -> derecha == nullptr){
-        removerHoja(nodoABorrar, padre);
-    } else if (nodoABorrar->izquierda == nullptr || nodoABorrar->derecha == nullptr){
-        removerConUnHijo(nodoABorrar,padre);
-    } else {
-        removerConDosHijos(nodoABorrar);
-    }
+    if (nodo->izquierda== nullptr && nodo -> derecha == nullptr)
+        removerHoja(nodo, padre);
+    else if (nodo->izquierda == nullptr || nodo->derecha == nullptr)
+        removerConUnHijo(nodo,padre);
+    else
+        removerConDosHijos(nodo);
     rebalancear(padre);
     _cardinal--;
 }
 template <class T>
 const T& ConjuntoAVL<T>::minimo() const {
-    NodoAVL<T>* SubArbolActual = _raiz;
-    while (SubArbolActual->izquierda != nullptr){
-        SubArbolActual=SubArbolActual->izquierda;
-    }
-    return SubArbolActual->clave;
+    NodoAVL<T>* nodo = _raiz;
+    while (nodo->izquierda != nullptr) nodo=nodo->izquierda;
+    return nodo->clave;
 }
 
 template <class T>
 const T& ConjuntoAVL<T>::maximo() const {
-    NodoAVL<T>* SubArbolActual = _raiz;
-    while (SubArbolActual->derecha != nullptr){
-        SubArbolActual=SubArbolActual->derecha;
-    }
-    return SubArbolActual->clave;
+    NodoAVL<T>* nodo = _raiz;
+    while (nodo->derecha != nullptr) nodo=nodo->derecha;
+    return nodo->clave;
 }
 
 template<class T>
@@ -178,24 +162,24 @@ void ConjuntoAVL<T>::destruir(NodoAVL<T> *raiz) {
 }
 
 template <class T>
-void ConjuntoAVL<T>::rebalancear(NodoAVL<T>* n){
-    definirBalanceo(n);
-    if (n->balanceo == -2) {
-        if (largo(n->izquierda->izquierda) >= largo(n->izquierda->derecha))
-            n = rotacionDerecha(n);
-        else n = rotacionIzqLuegoDer(n);
+void ConjuntoAVL<T>::rebalancear(NodoAVL<T>* nodo){
+    definirBalanceo(nodo);
+    if (nodo->balanceo == -2) {
+        if (largo(nodo->izquierda->izquierda) >= largo(nodo->izquierda->derecha))
+            nodo = rotacionDerecha(nodo);
+        else nodo = rotacionIzqLuegoDer(nodo);
     }
-    else if (n->balanceo == 2){
-        if (largo(n->derecha->derecha) >= largo(n->derecha->izquierda))
-            n = rotacionIzquierda(n);
-        else n = rotacionDerLuegoIzq(n);
+    else if (nodo->balanceo == 2){
+        if (largo(nodo->derecha->derecha) >= largo(nodo->derecha->izquierda))
+            nodo = rotacionIzquierda(nodo);
+        else nodo = rotacionDerLuegoIzq(nodo);
     }
-    if (n->padre != nullptr)  rebalancear(n->padre); else  _raiz = n;
+    if (nodo->padre != nullptr)  rebalancear(nodo->padre); else  _raiz = nodo;
 }
 
 template <class T>
-void ConjuntoAVL<T>::definirBalanceo(NodoAVL<T> *n){
-    n->balanceo = largo(n->derecha) - largo(n->izquierda);
+void ConjuntoAVL<T>::definirBalanceo(NodoAVL<T> *nodo){
+    nodo->balanceo = largo(nodo->derecha) - largo(nodo->izquierda);
 }
 
 template <class T>
@@ -206,9 +190,8 @@ NodoAVL<T>* ConjuntoAVL<T>::rotacionIzquierda (NodoAVL<T>* a){
     if (a->derecha != nullptr) a->derecha->padre = a;
     b->izquierda=a;
     a->padre=b;
-    if(b->padre != nullptr){
+    if(b->padre != nullptr)
         (b->padre->derecha == a) ? b->padre->derecha = b : b->padre->izquierda = b;
-    }
     definirBalanceo(a);
     definirBalanceo(b);
     return b;
@@ -221,116 +204,109 @@ NodoAVL<T>* ConjuntoAVL<T>::rotacionDerecha(NodoAVL<T>* a){
     if (a->izquierda != nullptr) a->izquierda->padre = a;
     b->derecha=a;
     a->padre=b;
-    if(b->padre != nullptr){
+    if(b->padre != nullptr)
         (b->padre->derecha == a) ? b->padre->derecha = b : b->padre->izquierda = b;
-    }
     definirBalanceo(a);
     definirBalanceo(b);
     return b;
 }
 
 template <class T>
-NodoAVL<T>* ConjuntoAVL<T>::rotacionIzqLuegoDer(NodoAVL<T> *n) {
-    n->izquierda = rotacionIzquierda(n->izquierda);
-    return rotacionDerecha(n);
+NodoAVL<T>* ConjuntoAVL<T>::rotacionIzqLuegoDer(NodoAVL<T> *nodo) {
+    nodo->izquierda = rotacionIzquierda(nodo->izquierda);
+    return rotacionDerecha(nodo);
 }
 
 template <class T>
-NodoAVL<T>* ConjuntoAVL<T>::rotacionDerLuegoIzq(NodoAVL<T> *n) {
-    n->derecha = rotacionDerecha(n->derecha);
-    return rotacionIzquierda(n);
+NodoAVL<T>* ConjuntoAVL<T>::rotacionDerLuegoIzq(NodoAVL<T> *nodo) {
+    nodo->derecha = rotacionDerecha(nodo->derecha);
+    return rotacionIzquierda(nodo);
 }
 
 template <class T>
 int ConjuntoAVL<T>::largo(NodoAVL<T>* n){
-    int res;
-    n == nullptr ? res = -1 : res = 1 + std::max(largo(n->izquierda), largo(n->derecha));
-    return res;
+    return n == nullptr ? -1 : 1 + max(largo(n->izquierda), largo(n->derecha));
 }
 
 template<class T>
-void ConjuntoAVL<T>::removerHoja(NodoAVL<T> *arClave, NodoAVL<T> *padreClave) {
-    if (padreClave == nullptr){
-        _raiz= nullptr;
-    } else if (padreClave->derecha==arClave){
-        padreClave->derecha= nullptr;
-    }
-    else if (padreClave->izquierda== arClave){
-        padreClave->izquierda= nullptr;
-    }
-    delete arClave;
+void ConjuntoAVL<T>::removerHoja(NodoAVL<T> *nodoBorrar, NodoAVL<T> *padreNodo) {
+    if (padreNodo == nullptr) _raiz= nullptr;
+    padreNodo->derecha==nodoBorrar ? padreNodo->derecha= nullptr : padreNodo->izquierda= nullptr;
+    delete nodoBorrar;
 }
 
-
-//Falta definir bien el delete acá.
 template<class T>
-void ConjuntoAVL<T>::removerConUnHijo(NodoAVL<T>* arClave, NodoAVL<T> *padreClave) {
-    if (padreClave == nullptr)//si es la raiz lo que quiero eliminar
-        (arClave->derecha== nullptr) ? _raiz=arClave->izquierda : _raiz= arClave->derecha;
+void ConjuntoAVL<T>::removerConUnHijo(NodoAVL<T>* nodoBorrar, NodoAVL<T> *padreNodo) {
+    if (padreNodo == nullptr)//si es la raiz lo que quiero eliminar
+        (nodoBorrar->derecha== nullptr) ? _raiz=nodoBorrar->izquierda : _raiz= nodoBorrar->derecha;
     else {
-        if (arClave->izquierda == nullptr)
-            (padreClave->derecha==arClave) ? padreClave-> derecha = arClave->derecha : padreClave->izquierda= arClave->derecha;
-        else if (arClave->derecha == nullptr)
-            (padreClave->derecha==arClave) ? padreClave-> derecha = arClave->izquierda : padreClave->izquierda= arClave->izquierda;
+        if (nodoBorrar->izquierda == nullptr) {
+            if (padreNodo->derecha == nodoBorrar) {
+                padreNodo->derecha = nodoBorrar->derecha;
+                padreNodo->derecha->padre = padreNodo;
+            } else {
+                padreNodo->izquierda = nodoBorrar->derecha;
+                padreNodo->izquierda->padre = padreNodo;
+            }
+        }
+        else {
+            if (padreNodo->derecha == nodoBorrar) {
+                padreNodo->derecha = nodoBorrar->izquierda;
+                padreNodo->derecha->padre = padreNodo;
+            } else {
+                padreNodo->izquierda = nodoBorrar->izquierda;
+                padreNodo->izquierda->padre = padreNodo;
+            }
+        }
     }
+    //Anulo los nodos asi puedo borrarlo.
+    nodoBorrar->derecha= nullptr;
+    nodoBorrar->izquierda= nullptr;
+    nodoBorrar->padre = nullptr;
+    delete nodoBorrar;
 }
 
+
+//TODO:Fijarse bien como funcionan los padres en esta funcion probablemente lo hace mal
+//Creo que al final solo una vez defino como padre a un nodo que es en el else asi que gane facil esto
 template <class T>
-void ConjuntoAVL<T>::removerConDosHijos(NodoAVL<T> *arClave) {
-    NodoAVL<T>* predMaximo = predecesorMaximo(arClave);
-    NodoAVL<T>* padreDeMaximo = subArbolPadre(arClave, predMaximo->clave);
-    arClave->clave=predMaximo->clave;
+void ConjuntoAVL<T>::removerConDosHijos(NodoAVL<T> *nodoBorrar) {
+    NodoAVL<T>* predMaximo = predecesorMaximo(nodoBorrar);
+    NodoAVL<T>* padreDeMaximo = predMaximo->padre;
+    nodoBorrar->clave=predMaximo->clave;
     if (predMaximo->izquierda== nullptr){ //me verifica si es hoja predMaximo
         if(padreDeMaximo->derecha == predMaximo){//verifica si el predMaximo es hijo derecho
             delete padreDeMaximo->derecha;
             padreDeMaximo->derecha = nullptr;
         }
-        else if (padreDeMaximo->izquierda == predMaximo){
+        else {
             delete padreDeMaximo->izquierda;
             padreDeMaximo->izquierda = nullptr;
         }
-    } else if (arClave->izquierda == predMaximo){
-        arClave->izquierda=predMaximo->izquierda;
+    }
+    else if (nodoBorrar->izquierda == predMaximo){
+        nodoBorrar->izquierda=predMaximo->izquierda;
         delete predMaximo;
     }
     else {
         padreDeMaximo->derecha=predMaximo->izquierda;
+        padreDeMaximo->derecha->padre = padreDeMaximo;
         delete predMaximo;
     }
 }
 
 template<class T>
-NodoAVL<T>* ConjuntoAVL<T>::subArbolPadre(NodoAVL<T> *raiz, const T &clave) {
-    NodoAVL<T> *subArbolActual = raiz;
-    NodoAVL<T> *raizDeSubArbol = nullptr;
-    while (subArbolActual != nullptr && subArbolActual->clave != clave) {
-        if (clave < subArbolActual->clave) {
-            raizDeSubArbol=subArbolActual;
-            subArbolActual = subArbolActual->izquierda;
-        }
-        else if (subArbolActual->clave < clave) {
-            raizDeSubArbol=subArbolActual;
-            subArbolActual = subArbolActual->derecha;
-        }
-    }
-    return raizDeSubArbol;
-}
-
-template<class T>
-NodoAVL<T>* ConjuntoAVL<T>::maximoDeArbol(NodoAVL<T> *n) {
-    NodoAVL<T>* SubArbolActual = n;
-    while (SubArbolActual->derecha != nullptr){
-        SubArbolActual=SubArbolActual->derecha;
-    }
-    return SubArbolActual;
+NodoAVL<T>* ConjuntoAVL<T>::maximoDeArbol(NodoAVL<T> *nodoRaiz) {
+    NodoAVL<T>* nodo = nodoRaiz;
+    while (nodo->derecha != nullptr) nodo=nodo->derecha;
+    return nodo;
 }
 
 template <class T>
-NodoAVL<T>* ConjuntoAVL<T>::predecesorMaximo(NodoAVL<T> *abb){
-    NodoAVL<T>* predMaximo = maximoDeArbol(abb->izquierda);
+NodoAVL<T>* ConjuntoAVL<T>::predecesorMaximo(NodoAVL<T> *nodo){
+    NodoAVL<T>* predMaximo = maximoDeArbol(nodo->izquierda);
     return predMaximo;
 }
-
 
 template<class T>
 void ConjuntoAVL<T>::printAVL(NodoAVL<T>* root, int space) {
@@ -357,8 +333,10 @@ int main(){
         c.insertar(i);
         elementos.push_back(i);
     }
+    cout << c.maximo() << endl;
+    cout << c.minimo() << endl;
     c.borrar(8);
+    c.borrar(5);
+    c.borrar(2);
     c.printAVL();
-  //  std::cout << c.cardinal() << std::endl;
- //   c.printAVL();
 }
